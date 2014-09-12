589e273d247942537855a7b7004fd13b46b6962f
aviram120 <aviram120@walla.com>
2014-09-12 10:33:25 +0300

    Create soundPP.h

---
+++ soundPP.h
@@ -0,0 +1,15 @@
+
+#pragma once
+
+
+void playSoundAvi(const double &num);
+void playMillion(int x);
+void play100k(int x);
+void play10k(int x);
+void playK(int x);
+void numberOfTheSoundHundred(int x);
+void numberOfTheSoundTen(int x);
+void numberOfTheSoundOne(int x);
+void numberbetween19To11(int x);
+
+
